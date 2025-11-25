554e433c43697212aa527261ab766769be095df3
Ali Ashraf <aaaaaa30101975@gmail.com>
2025-11-25 10:50:19 +0200

    feature: add timline icons

--- src/components/features/about-section/NavigationBar.tsx
+++ src/components/features/about-section/NavigationBar.tsx
@@ -1,3 +1,5 @@
+import { LuMouse } from "react-icons/lu";
+
 const sections = [
   {
     id: "about",
@@ -37,15 +39,15 @@ export default function NavigationBar({
         <button
           key={section.id}
           onClick={() => onNavigate(index)}
-          className={`ps-4 border-s-2 text-sm md:text-md transition-all duration-300 text-left ${
-            activeIndex === index
+          className={`ps-4 border-s-2 text-sm md:text-md transition-all duration-300 text-left ${activeIndex === index
               ? "text-white font-semibold border-blue-500"
               : "text-gray-400 hover:text-white border-transparent"
-          }`}
+            }`}
         >
           {section.label}
         </button>
       ))}
+      <div className="hidden md:flex md:justify-center animate-bounce text-white text-lg"><LuMouse /></div>
     </nav>
   );
 }
--- src/components/features/timeline-section/TimeLine.tsx
+++ src/components/features/timeline-section/TimeLine.tsx
@@ -1,6 +1,6 @@
 import FolderShape from "./FolderShape";
 import { CiSearch } from "react-icons/ci";
-import { FaAddressBook, FaRegBookmark } from "react-icons/fa";
+import { FaAddressBook, FaGraduationCap, FaRegBookmark } from "react-icons/fa";
 import { CgBot } from "react-icons/cg";
 import ImageCard from "./ImageCard";
 import gsap from "gsap";
@@ -55,80 +55,119 @@ export default function TimeLine() {
           start: "top 80%",
         }
       })
-      gsap.from(".image-card", {
-        x: 100,
-        opacity: 0,
-        duration: 1.2,
-        ease: "power4.out",
+
+      // gsap.fromTo(".timeline-header", {
+      //   x: 40,
+      //   opacity: 0,
+      // }, {
+      //   x: 0,
+      //   opacity: 1,
+      //   duration: 0.5,
+      //   scrollTrigger: {
+      //     trigger: ".timeline-header",
+      //     start: "top 80%",
+      //   }
+      // })
+
+      gsap.timeline({
         scrollTrigger: {
-          trigger: ".image-card",
-          start: "top 10%",
+          trigger: ".timeline-label",
+          start: "top 80%",
         }
-      });
+      }).fromTo(".timeline-label",{
+        x: -40,
+        opacity: 0,
+      }, {
+        x: 0,
+        opacity: 1,
+        duration: 0.5,
+      }).fromTo(".timeline-icon", {
+        x: 40,
+        opacity: 0,
+      }, {
+        x: 0,
+        opacity: 1,
+        duration: 0.5,
+      })
 
-      const folders = document.querySelectorAll(".folder");
-
-      folders.forEach((folder, index) => {
-        gsap.from(folder, {
-          y: 60 * (index + 1),
-          opacity: 0,
-          scrollTrigger: {
-            trigger: text,
-            start: "top 80%",
-          }
-        });
-      });
-      
+  gsap.from(".image-card", {
+    x: 100,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power4.out",
+    scrollTrigger: {
+      trigger: ".image-card",
+      start: "top 10%",
     }
+  });
+
+  const folders = document.querySelectorAll(".folder");
+
+  folders.forEach((folder, index) => {
+    gsap.from(folder, {
+      y: 60 * (index + 1),
+      opacity: 0,
+      scrollTrigger: {
+        trigger: text,
+        start: "top 80%",
+      }
+    });
+  });
+
+}
   }, []);
 
-  const folders = [
-    {
-      header: "Admissions",
-      description:
-        "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
-      icon: CiSearch,
-    },
-    {
-      header: "Academics",
-      description:
-        "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
-      icon: FaAddressBook,
-    },
-    {
-      header: "Research",
-      description:
-        "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
-      icon: FaRegBookmark,
-    },
-    {
-      header: "Support Services",
-      description:
-        "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
-      icon: CgBot,
-    },
-  ];
+const folders = [
+  {
+    header: "Admissions",
+    description:
+      "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
+    icon: CiSearch,
+  },
+  {
+    header: "Academics",
+    description:
+      "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
+    icon: FaAddressBook,
+  },
+  {
+    header: "Research",
+    description:
+      "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
+    icon: FaRegBookmark,
+  },
+  {
+    header: "Support Services",
+    description:
+      "Join our vibrant community of learners Join our vibrant community of learners  Join our vibrant community of learners  Join our vibrant community of learners.",
+    icon: CgBot,
+  },
+];
 
-  return (
-    <div className="bg-background min-h-screen  relative container timeline py-10">
-      <h2 className="text-3xl font-bold mb-4 text-center timeline-header">Explore Our Features</h2>
-
-      <p ref={textRef} className="text-gray-700 max-w-lg text-center mx-auto mb-10">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam modi
-        natus nesciunt porro, optio dolore!
-      </p>
-
-      <div className="py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {folders.map((folder, index) => (
-          <FolderShape
-            key={index}
-            header={folder.header}
-            description={folder.description}
-            Icon={folder.icon}
-          />
-        ))}
-        <ImageCard />
-      </div>
+return (
+  <div className="bg-background min-h-screen  relative container timeline py-10">
+    <div className="flex items-center justify-between">
+      <div className="border-s-4 border-s-accent ps-4 text font-semibold timeline-label">How It Works</div>
+      <div><FaGraduationCap className="text-4xl md:text-6xl timeline-icon" /></div>
     </div>
-  );
+    <h2 className="text-3xl font-bold mb-4 text-center timeline-header">Explore Our Features</h2>
+
+    <p ref={textRef} className="text-gray-700 max-w-lg text-center mx-auto mb-10">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam modi
+      natus nesciunt porro, optio dolore!
+    </p>
+
+    <div className="py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {folders.map((folder, index) => (
+        <FolderShape
+          key={index}
+          header={folder.header}
+          description={folder.description}
+          Icon={folder.icon}
+        />
+      ))}
+      <ImageCard />
+    </div>
+  </div>
+);
 }
